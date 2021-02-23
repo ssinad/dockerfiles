bcc21bc6aeb9291b2de0d8c8512dd560e58cdc4a
ssinad <27953063+ssinad@users.noreply.github.com>
2021-02-22 21:46:48 -0700

    I'm bored

--- .github/scripts/docker-build-push.sh
+++ .github/scripts/docker-build-push.sh
@@ -4,8 +4,8 @@ USERNAME=$1
 dir=$2
 GITHUB_SHA=$3
 TAGS="-t $USERNAME/$dir:$GITHUB_SHA -t $USERNAME/$dir:latest"
-docker pull $USERNAME/$dir:latest \
-          || docker build --no-cache --build-arg BUILDKIT_INLINE_CACHE=1 $TAGS $dir 
-          docker build --cache-from $USERNAME/$dir $TAGS $dir 
-          docker push $USERNAME/$dir:$GITHUB_SHA
-          docker push $USERNAME/$dir:latest
+    docker pull $USERNAME/$dir:latest \
+||  docker build --no-cache --build-arg BUILDKIT_INLINE_CACHE=1 $TAGS $dir 
+    docker build --cache-from $USERNAME/$dir $TAGS $dir 
+    docker push $USERNAME/$dir:$GITHUB_SHA
+    docker push $USERNAME/$dir:latest
