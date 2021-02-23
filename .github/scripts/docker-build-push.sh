1d3634d4ea4e1e3135896106ca61b243052a31f6
ssinad <27953063+ssinad@users.noreply.github.com>
2021-02-22 20:47:09 -0700

    Take TAGS variable inside

--- .github/scripts/docker-build-push.sh
+++ .github/scripts/docker-build-push.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 USERNAME=$1
 dir=$2
-TAGS=$3
-GITHUB_SHA=$4
+GITHUB_SHA=$3
+TAGS="-t $USERNAME/$dir:$GITHUB_SHA -t $USERNAME/$dir:latest"
 docker pull $USERNAME/$dir:latest \
           || docker build --no-cache --build-arg BUILDKIT_INLINE_CACHE=1 $TAGS $dir 
           docker build --cache-from $USERNAME/$dir $TAGS $dir 
